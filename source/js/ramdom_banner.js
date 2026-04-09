b2681fa05f5dce0b2787de82cd13cb9f3c627d96
pinbored <guotj3@mail2.sysu.edu.cn>
2026-04-09 17:27:01 +0800

    v2026.4.9.1 Add filtering functionality for ramdom banner

--- source/js/ramdom_banner.js
+++ source/js/ramdom_banner.js
@@ -1,3 +1,6 @@
+const Home = window.location.pathname === '/';
+const OtherPage = window.location.pathname.includes('/about/') || window.location.pathname.includes('/archives/') || window.location.pathname.includes('/categories/') || window.location.pathname.includes('/tags/') || window.location.pathname.includes('/about/');
+if (banner && (Home || OtherPage)) {
 const imgs = [
 	"/global/banner/sysu100.jpg",
 	"/global/banner/1.jpg",
@@ -6,7 +9,7 @@ const imgs = [
 	"/global/banner/4.jpg",
 	"/global/banner/5.jpg",
 ]
-
 const luck_img = imgs[Math.floor(Math.random() * imgs.length)]
 const banner = document.getElementById('banner')
 banner.style.background = `url(${luck_img}) center center / cover no-repeat`
+}
